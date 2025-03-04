cdb1bdabd704ee40f3b37a17e3aa44011d0afa1c
zhouyu <zhouyu-jt@163.com>
2025-03-04 16:27:51 +0800

    feat(benchmarks): configure rule files

--- benchmarks/srsecurity/main.cc
+++ benchmarks/srsecurity/main.cc
@@ -109,11 +109,36 @@ int main(int argc, char* argv[]) {
       "test/test_data/waf-conf/base/engin-setup.conf",
       "test/test_data/waf-conf/base/crs-setup.conf",
       "test/test_data/waf-conf/coreruleset/rules/REQUEST-901-INITIALIZATION.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-905-COMMON-EXCEPTIONS.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-911-METHOD-ENFORCEMENT.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-913-SCANNER-DETECTION.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-920-PROTOCOL-ENFORCEMENT.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-921-PROTOCOL-ATTACK.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-922-MULTIPART-ATTACK.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-930-APPLICATION-ATTACK-LFI.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-931-APPLICATION-ATTACK-RFI.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-932-APPLICATION-ATTACK-RCE.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-933-APPLICATION-ATTACK-PHP.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-934-APPLICATION-ATTACK-GENERIC.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-941-APPLICATION-ATTACK-XSS.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/"
+      // "REQUEST-943-APPLICATION-ATTACK-SESSION-FIXATION.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-944-APPLICATION-ATTACK-JAVA.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/REQUEST-949-BLOCKING-EVALUATION.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-950-DATA-LEAKAGES.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-951-DATA-LEAKAGES-SQL.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-952-DATA-LEAKAGES-JAVA.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-953-DATA-LEAKAGES-PHP.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-954-DATA-LEAKAGES-IIS.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-955-DATA-LEAKAGES-APACHE.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-959-BLOCKING-EVALUATION.conf",
+      // "test/test_data/waf-conf/coreruleset/rules/RESPONSE-980-CORRELATION.conf",
   };
   for (auto& rule_file : rule_files) {
     result = engine.loadFromFile(rule_file);
     if (!result.has_value()) {
-      std::cout << "Load rules error: " << "[" << rule_file << "] " << result.error() << std::endl;
+      std::cout << "Load rules error: " << result.error() << std::endl;
       return 1;
     }
   }
