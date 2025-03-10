5acf2d0c2343dd86f926f8be6db1b9b06f458af6
zhouyu <zhouyu-jt@163.com>
2025-03-10 17:21:28 +0800

    feat(operator): add within operator

--- src/operator/within.h
+++ src/operator/within.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "operator_base.h"
-
+/**
+ * Returns true if the input value (the needle) is found anywhere within the @within parameter (the
+ * haystack). Macro expansion is performed on the parameter string before comparison.
+ */
 namespace SrSecurity {
 namespace Operator {
 class Within : public OperatorBase {
@@ -9,16 +12,65 @@ class Within : public OperatorBase {
 
 public:
   Within(std::string&& literal_value, bool is_not)
-      : OperatorBase(std::move(literal_value), is_not) {}
+      : OperatorBase(std::move(literal_value), is_not) {
+    // Split the literal value into tokens.
+    tokens_ = SplitTokens(literal_value_);
+  }
 
   Within(const std::shared_ptr<Macro::MacroBase> macro, bool is_not)
       : OperatorBase(macro, is_not) {}
 
 public:
   bool evaluate(Transaction& t, const Common::Variant& operand) const override {
-    assert(false);
-    throw "Not implemented!";
+    if (!IS_STRING_VIEW_VARIANT(operand)) [[unlikely]] {
+      return false;
+    }
+
+    bool find = false;
+    if (!macro_) [[likely]] {
+      for (auto token : tokens_) {
+        if (std::get<std::string_view>(operand).find(token) != std::string_view::npos) {
+          find = true;
+          break;
+        }
+      }
+      return is_not_ ^ find;
+    } else {
+      MACRO_EXPAND_STRING_VIEW(macro_value);
+      std::vector<std::string_view> tokens = SplitTokens(macro_value);
+      for (auto token : tokens) {
+        if (std::get<std::string_view>(operand).find(token) != std::string_view::npos) {
+          find = true;
+          break;
+        }
+      }
+      return is_not_ ^ find;
+    }
   }
+
+private:
+  std::vector<std::string_view> SplitTokens(std::string_view value) const {
+    // Split by space
+    constexpr char delimiter = ' ';
+
+    std::vector<std::string_view> tokens;
+    size_t pos = 0;
+    size_t next_pos = 0;
+
+    while ((next_pos = value.find(delimiter, pos)) != std::string_view::npos) {
+      tokens.emplace_back(value.substr(pos, next_pos - pos));
+      pos = next_pos + 1;
+    }
+
+    if (pos < value.size()) {
+      tokens.emplace_back(value.substr(pos));
+    }
+
+    return tokens;
+  }
+
+private:
+  std::vector<std::string_view> tokens_;
 };
 } // namespace Operator
 } // namespace SrSecurity
--- test/parser/rule_operator.cc
+++ test/parser/rule_operator.cc
@@ -87,5 +87,56 @@ TEST_F(RuleOperatorTest, ipMatch) {
   EXPECT_TRUE(t->hasVariable("ipv6_mask_true"));
   EXPECT_FALSE(t->hasVariable("ipv6_mask_false"));
 }
+
+TEST_F(RuleOperatorTest, within) {
+  const std::string directive =
+      R"(SecAction "phase:1,setvar:tx.foo=helloworld"
+      SecRule TX:foo "@within hello" "id:1,phase:1,setvar:'tx.true1'"
+      SecRule TX:foo "@within hello " "id:2,phase:1,setvar:'tx.true2'"
+      SecRule TX:foo "@within hello1 world" "id:3,phase:1,setvar:'tx.true3'"
+      SecRule TX:foo "@within hello1" "id:4,phase:1,setvar:'tx.false1'"
+      SecRule TX:foo "@within hello1 world1" "id:5,phase:1,setvar:'tx.false2'")";
+
+  auto result = engine_.load(directive);
+  engine_.init();
+  auto t = engine_.makeTransaction();
+  ASSERT_TRUE(result.has_value());
+
+  t->processRequestHeaders(nullptr, nullptr);
+  EXPECT_TRUE(t->hasVariable("true1"));
+  EXPECT_TRUE(t->hasVariable("true2"));
+  EXPECT_TRUE(t->hasVariable("true3"));
+  EXPECT_FALSE(t->hasVariable("false1"));
+  EXPECT_FALSE(t->hasVariable("false2"));
+}
+
+TEST_F(RuleOperatorTest, withinWithMacro) {
+  const std::string directive =
+      R"(SecAction "phase:1,setvar:tx.foo=helloworld"
+      SecAction "phase:1,setvar:tx.v1=hello"
+      SecAction "phase:1,setvar:tx.v2=hello "
+      SecAction "phase:1,setvar:tx.v3=hello1 world"
+      SecAction "phase:1,setvar:tx.v4=hello1"
+      SecAction "phase:1,setvar:tx.v5=hello1 world1"
+      SecRule TX:foo "@within %{tx.v1}" "id:1,phase:1,setvar:'tx.true1'"
+      SecRule TX:foo "@within %{tx.v2}" "id:2,phase:1,setvar:'tx.true2'"
+      SecRule TX:foo "@within %{tx.v3}" "id:3,phase:1,setvar:'tx.true3'"
+      SecRule TX:foo "@within %{tx.v4} %{tx.v1}" "id:3,phase:1,setvar:'tx.true4'"
+      SecRule TX:foo "@within %{tx.v4}" "id:4,phase:1,setvar:'tx.false1'"
+      SecRule TX:foo "@within %{tx.v5}" "id:5,phase:1,setvar:'tx.false2'")";
+
+  auto result = engine_.load(directive);
+  engine_.init();
+  auto t = engine_.makeTransaction();
+  ASSERT_TRUE(result.has_value());
+
+  t->processRequestHeaders(nullptr, nullptr);
+  EXPECT_TRUE(t->hasVariable("true1"));
+  EXPECT_TRUE(t->hasVariable("true2"));
+  EXPECT_TRUE(t->hasVariable("true3"));
+  EXPECT_TRUE(t->hasVariable("true4"));
+  EXPECT_FALSE(t->hasVariable("false1"));
+  EXPECT_FALSE(t->hasVariable("false2"));
+}
 } // namespace Parser
 } // namespace SrSecurity
