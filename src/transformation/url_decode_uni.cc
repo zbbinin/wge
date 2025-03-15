9fb4559d91de1240a6674d752c13bcdfa16f0339
zhouyu <zhouyu-jt@163.com>
2025-03-16 04:34:38 +0800

    refactor(transformation): refactor UrlDecodeUni
    refactor UrlDecodeUni to use boost url library for decoding url encoded
    strings

--- src/transformation/url_decode_uni.cc
+++ src/transformation/url_decode_uni.cc
@@ -1,32 +1,12 @@
 #include "url_decode_uni.h"
 
-#include "hex_decode.h"
+#include <boost/url.hpp>
 
 namespace SrSecurity {
 namespace Transformation {
 std::string UrlDecodeUni::evaluate(const void* data, size_t data_len) const {
-  std::string result;
-  result.reserve(data_len);
-  HexDecode hex_decode;
-  for (size_t i = 0; i < data_len; ++i) [[likely]] {
-    const char& ch = static_cast<const char*>(data)[i];
-    switch (ch) {
-    case '%':
-      if (i + 2 < data_len) [[likely]] {
-        result += hex_decode.evaluate(static_cast<const char*>(data) + i + 1, 2);
-        i += 2;
-      }
-      break;
-    case '+':
-      result += ' ';
-      break;
-    [[likely]] default:
-      result += ch;
-      break;
-    }
-  }
-
-  return result;
+  boost::urls::pct_string_view pct_str(reinterpret_cast<const char*>(data), data_len);
+  return pct_str.decode();
 }
 } // namespace Transformation
 } // namespace SrSecurity
