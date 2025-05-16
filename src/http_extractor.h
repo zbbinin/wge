cbbacd08aa3d74c7554a489c878e2030cc73d495
zhouyu <zhouyu-jt@163.com>
2025-05-16 10:54:34 +0800

    Revert "refactor: declare parameter need lower case"
    This reverts commit 86a1a29fddc1df10e53a67fa25079c625b9af5b3.

--- src/http_extractor.h
+++ src/http_extractor.h
@@ -27,19 +27,18 @@ namespace Wge {
 
 /**
  * Header find function.
- * @param lower_key the header key with lower case.
+ * @param key the header key.
  * @return the header value. if the header does not exist, return empty string_view.
  */
-using HeaderFind = std::function<std::string_view(std::string_view lower_key)>;
+using HeaderFind = std::function<std::string_view(const std::string& key)>;
 
 /**
  * Header traversal callback.
- * @param lower_key the header key with lower case.
+ * @param key the header key.
  * @param value the header value.
  * @return true if continue traversal, false if stop traversal.
  */
-using HeaderTraversalCallback =
-    std::function<bool(std::string_view lower_key, std::string_view value)>;
+using HeaderTraversalCallback = std::function<bool(std::string_view key, std::string_view value)>;
 
 /**
  * Header traversal function.
