a41b1c07a96f90d12e65ce3e1dcbb4333ef31356
zhouyu <zhouyu-jt@163.com>
2025-08-07 12:02:40 +0800

    feat(tool): support L1 cache performance analysis

--- tools/perf_wge_benchmark_cache_misses.sh
+++ tools/perf_wge_benchmark_cache_misses.sh
@@ -2,4 +2,8 @@
 if [[ ! -d perf ]]; then
   mkdir perf
 fi
-LD_LIBRARY_PATH=/usr/local/lib64 perf stat -e cache-references,cache-misses -- build/release-with-debug-info/benchmarks/wge/wge_benchmark $@
+LD_LIBRARY_PATH=LD_LIBRARY_PATH=/usr/local/lib64 perf stat \
+  -e L1-dcache-loads,L1-dcache-load-misses,L1-icache-load-misses \
+  -e cache-references,cache-misses \
+  -e cycles,instructions,branches,branch-misses \
+  -- build/release-with-debug-info/benchmarks/wge/wge_benchmark $@
