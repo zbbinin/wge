e763b8d136209c7816117f5aa97e721c4842e6d9
zhouyu <zhouyu-jt@163.com>
2025-09-18 18:48:47 +0800

    fix(common): fix logging initialization error

--- src/common/log.cc
+++ src/common/log.cc
@@ -30,8 +30,10 @@ std::shared_ptr<spdlog::logger> Log::logger_holder_;
 void Log::init(spdlog::level::level_enum level, const std::string& log_file) {
   if (!log_file.empty()) {
     logger_holder_ =
-        spdlog::rotating_logger_mt("rotating_logger", "wge/wge.log", 1024 * 1024 * 100, 3);
+        spdlog::rotating_logger_mt("rotating_logger", log_file.c_str(), 1024 * 1024 * 100, 3);
     logger_ = logger_holder_.get();
+  } else {
+    logger_ = spdlog::default_logger_raw();
   }
 
   spdlog::set_level(level);
